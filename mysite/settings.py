b28513579ef27f2fddf1f1779fe5482de62ee067
fati <fati.mansoory@gmail.com>
2023-10-15 19:06:37 +0330

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,25 +121,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fatimz567$ads',
-        'USER': 'fatimz567',
-        'PASSWORD': 'F@ti2000',
-        'HOST': 'fatimz567.mysql.pythonanywhere-services.com',
-         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'fatimz567$ads',
+#         'USER': 'fatimz567',
+#         'PASSWORD': 'F@ti2000',
+#         'HOST': 'fatimz567.mysql.pythonanywhere-services.com',
+#          'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 
 
 # Password validation
